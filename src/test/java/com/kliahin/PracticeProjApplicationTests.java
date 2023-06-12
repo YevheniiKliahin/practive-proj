918df89b05e24bab86261fdba76274bec1b25120
eklyagin <rkzuby@gmail.com>
2023-06-12 14:05:00 +0300

    init

---
+++ src/test/java/com/kliahin/PracticeProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kliahin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
